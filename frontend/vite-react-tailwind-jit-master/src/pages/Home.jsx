626e944aa2667ffb014a5baca217d41eaa78a341
Hastansh <hastu4123@gmail.com>
2023-03-10 22:48:01 +0530

    initial commit

---
+++ frontend/vite-react-tailwind-jit-master/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="fixed left-0 right-0 top-200 h- shadow-md border-b-2 border-gray-100 bg-gray-900">
+      <h1>This is home page.</h1>
+      <section className="home-section">
+      <div className="container">
+        <h1>Welcome to My Website</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <button className="btn btn-primary">Get Started</button>
+      </div>
+    </section>
+    </div>
+  )
+}
+
+export default Home
